82fb5945d142f4d17e9cc2e0c58277fdd8a95192
Simon <lindersi@vtxmail.ch>
2022-05-15 01:20:44 +0200

    Konturerkennung, Rechtecke legen.

--- vision.py
+++ vision.py
@@ -20,11 +20,11 @@ def analyse(img):
     img_orig = cv.imread(img_path)
     if img_orig is None:
         sys.exit("Could not read the image.")
-    # show(img_orig, 1)
+    # show(img_orig, 2)
 
     # Graustufen
     img_gray = cv.imread(img, cv.IMREAD_GRAYSCALE)
-    # show(img_gray, 1)
+    # show(img_gray, 2)
 
     # Binarisieren
     # ret, img_bin = cv.threshold(img_gray, 170, 255, cv.THRESH_BINARY)
@@ -33,15 +33,43 @@ def analyse(img):
     # Otsu's thresholding after Gaussian filtering
     img_gray = cv.GaussianBlur(img_gray, (5, 5), 0)
     ret, img_bin = cv.threshold(img_gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    show(img_bin, 1)
+    # show(img_bin, 2)
 
     # Konturen erkennen
     contours, hierarchy = cv.findContours(img_bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(img_orig, contours, -1, (255, 0, 255), 3)
+    # cv.drawContours(img_orig, contours, -1, (255, 0, 255), 5)
+
+    # Konturen bewerten und Karten suchen (9 oder 12 grosse (viereckige) Flächen)
+    areas = []
+    for cont in contours:
+        area = (cv.contourArea(cont))
+        areas.append(area)
+    cards = []
+    i = 0
+    for a in areas:
+        if a > max(areas) * 0.5:
+            cards.append(contours[i])
+        i += 1
+    # cv.drawContours(img_orig, cards, -1, (0, 255, 0), 10)
+
+    # return img_orig
+
+    # Konturen mit viereckiger Form finden
+    boxes = []
+    for cnt in contours:
+        rect = cv.minAreaRect(cnt)
+        box = cv.boxPoints(rect)
+        boxes.append(np.int0(box))
+
+    cv.drawContours(img_orig, boxes, -1, (0, 0, 255), 10)
 
     return img_orig
 
-
-img_path = "Photos/Set-2_dunkel-lose-gerade.jpg"
-img = analyse(img_path)
-show(img, 9)
+if __name__ == '__main__':
+    # img_path = "Photos/Set-1_weiss-eng-gerade.jpg"
+    img_path = "Photos/Set-2_dunkel-lose-gerade.jpg"  # Idealbild für den Anfang
+    # img_path = "Photos/Set-3_dunkel-lose-gerade.jpg"
+    # img_path = "Photos/Set-4_dunkel-lose-schief.jpg"
+    # img_path = "Photos/Set-5_dunkel-ungeordnet.jpg"
+    img = analyse(img_path)
+    show(img, -1)
